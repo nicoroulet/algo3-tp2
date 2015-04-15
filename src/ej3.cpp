541d97cb68846214ea4895844b37be4a21758750
zappa1 <jp_rulo@hotmail.com>
2015-04-15 19:38:41 -0300

    ej3 anda, Makefile y un test

--- src/ej3.cpp
+++ src/ej3.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
-typedef struct {
+using namespace std;
+struct conexion {
 	int a, b, c; // pozo a, pozo b, costo
-	operator<(c2) {return c<c2.c}
-} conexion;
+	bool operator<(const conexion c2) const {return c<c2.c;}
+};
 
 int main() {
 	int n, m, C;
-	cin << n << m << C; // pozos, conexiones, costo refineria
+	cin >> n >> m >> C; // pozos, conexiones, costo refineria
 
 	vector<conexion> conexiones;
 
 	for (int i = 0; i < m; ++i)
 	{
 		conexion temp;
-		cin << temp.a << temp.b << temp.c;
-		if (c<C) conexiones.push_back(temp);
+		cin >> temp.a >> temp.b >> temp.c;
+		if (temp.c<C) conexiones.push_back(temp);
 	}
-	sort(conexiones);
+	sort(conexiones.begin(), conexiones.end());
 
 	int ultimo_arbol=0;
 	vector<int> arboles(n,-1); // iesima posicion indica en que numero de arbol esta el nodo i
 	vector<conexion> conexiones_finales;
+	int costo_final=0;
 	for (auto it = conexiones.begin(); it < conexiones.end(); ++it)
 	{
 		/* opciones: a y b no estan en ningun arbol
@@ -30,27 +33,46 @@ int main() {
 					 los dos estan en el mismo arbol
 					 los dos estan en arboles diferentes
 		*/
-		if (arbol[it->a] == -1 && arbol[it->b] == -1) { // si ningun nodo esta en ningun arbol
-			arbol[it->a] = arbol[it->b] = ultimo_arbol++; // wowowowowo
+		if (arboles[it->a] == -1 && arboles[it->b] == -1) { // si ningun nodo esta en ningun arbol
+			arboles[it->a] = arboles[it->b] = ultimo_arbol++; // wowowowowo
 			conexiones_finales.push_back(*it);
+			costo_final += it->c;
 		}
-		else if (arbol[it->a] != -1 && arbol[it->b] == -1) { // si b no esta en ningun arbol y a si
-			arbol[it->b] = arbol[it->a];
+		else if (arboles[it->a] != -1 && arboles[it->b] == -1) { // si b no esta en ningun arbol y a si
+			arboles[it->b] = arboles[it->a];
 			conexiones_finales.push_back(*it);
+			costo_final += it->c;
 		}
-		else if (arbol[it->a] == -1 && arbol[it->b] != -1) { // si a no esta en ningun arbol y b si
-			arbol[it->a] = arbol[it->b];
+		else if (arboles[it->a] == -1 && arboles[it->b] != -1) { // si a no esta en ningun arbol y b si
+			arboles[it->a] = arboles[it->b];
 			conexiones_finales.push_back(*it);
+			costo_final += it->c;
 		}
-		else if (arbol[it->a] != arbol[it->b]) {	// si a y b estan en arboles diferentes, los mergeamos
-			int temp = arbol[it->b];
+		else if (arboles[it->a] != arboles[it->b]) {	// si a y b estan en arboles diferentes, los mergeamos
+			int temp = arboles[it->b];
 			for (int i = 0; i < n; ++i)
 			{
-				if (arbol[i] == temp) arbol[i] = arbol[it->a];
+				if (arboles[i] == temp) arboles[i] = arboles[it->a];
 			}
 			conexiones_finales.push_back(*it);
+			costo_final += it->c;
 		}
 		
 	}
-
+	vector<int> refinerias;
+	int i = 0;
+	for (auto it = arboles.begin(); it != arboles.end(); ++it, ++i)
+	{
+		if (find(arboles.begin(), it, *it) == it || (*it == -1)) refinerias.push_back(i);
+	}
+	costo_final += refinerias.size() * C;
+	cout << costo_final << " " << refinerias.size() << " " << conexiones_finales.size() << endl;
+	for (int i = 0; i < refinerias.size(); ++i)
+	{
+		cout << refinerias[i] << endl;
+	}
+	for (int i = 0; i < conexiones_finales.size(); ++i)
+	{
+		cout << conexiones_finales[i].a << " " << conexiones_finales[i].b << endl;
+	}
 }
