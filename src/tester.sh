56300b92bf9d652c0eb3a8aa5017308399db013a
smcsicardi <smcsicardi@gmail.com>
2015-04-28 10:50:07 -0300

    Algo mas lindo para el tester

--- src/tester.sh
+++ src/tester.sh
@@ -1,36 +1,5 @@
-NAMES="
-001
-002
-003
-004
-005
-006
-007
-008
-009
-010
-011
-012
-013
-014
-015
-016
-017
-018
-019
-020
-021
-022
-023
-024
-025
-026
-027
-028
-029"
-
-for f in $NAMES
+for f in tests.tp2.ej1.v1.1/*.in
 do 
-echo "Testeando $f..."
-diff -Z <(./ej1 < tests.tp2.ej1.v1.1/$f.in) tests.tp2.ej1.v1.1/$f.out
+echo "Testeando ${s%.*}..."
+diff -Z <(./ej1 < tests.tp2.ej1.v1.1/${s%.*}.in) tests.tp2.ej1.v1.1/${s%.*}.out
 done
